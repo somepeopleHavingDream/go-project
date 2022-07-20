ba41040700ab040879782ec7a9351c57273ca330
yangxin <1393003255@qq.com>
2022-07-20 20:44:52 +0800

    feat: 使用flag获取复杂参数

--- text/flag/main.go
+++ text/flag/main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 )
 
+func style1() {
+	// fmt.Println(os.Args)
+
+	// 格式化定义
+	methodPtr := flag.String("method", "default", "method of sample")
+	valuePtr := flag.Int("value", -1, "value of sample")
+
+	// 解析
+	flag.Parse()
+
+	fmt.Println(*methodPtr, *valuePtr)
+}
+
+func style2() {
+	var method string
+	var value int
+
+	flag.StringVar(&method, "method", "default", "method of sample")
+	flag.IntVar(&value, "value", -1, "value of sample")
+
+	flag.Parse()
+
+	fmt.Println(method, value)
+}
+
 func main() {
-	fmt.Println(os.Args)
+	style2()
 }
